487fc7a1985d39e187d7f9bafe3d453381e99e68
sammyseetharam <90780132+sammyseetharam@users.noreply.github.com>
2022-08-25 11:02:04 -0500

    working towards light mode
    finish at home add light mode through new activity

---
+++ app/src/main/java/com/example/mycalculatorapp/lightMode.java
@@ -0,0 +1,14 @@
+package com.example.mycalculatorapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class lightMode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_light_mode);
+    }
+}
